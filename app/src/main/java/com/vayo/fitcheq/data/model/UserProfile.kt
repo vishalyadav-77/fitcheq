14260306ba6661e7bf32924001e8f367a4a110e4
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-03-23 22:26:22 +0530

    UserProfile DataClass created

---
+++ app/src/main/java/com/vayo/fitcheq/data/model/UserProfile.kt
@@ -0,0 +1,23 @@
+package com.vayo.fitcheq.data.model
+
+data class UserProfile(
+    val uId: String = "",
+    val name: String = "",
+    val gender: String = "", // "Male" or "Female"
+    val occupation: String = "", // "College Student", working professional, school student , other
+    val ageGroup: AgeGroup = AgeGroup.UNSPECIFIED, // Default value
+    val preferPlatform: PreferPlatform = PreferPlatform.moderate //default value
+)
+
+enum class AgeGroup(val displayName: String) {
+    BELOW_18("Below 18"),
+    AGE_18_25("18-25"),
+    AGE_25_30("25-30"),
+    AGE_30_PLUS("30+"),
+    UNSPECIFIED("Unspecified") // For cases where the user hasn't selected an age
+}
+enum class PreferPlatform(val displayName: String) {
+    cheap("meesho"),
+    moderate("Myntra"),
+    expensive("Zara")
+}
