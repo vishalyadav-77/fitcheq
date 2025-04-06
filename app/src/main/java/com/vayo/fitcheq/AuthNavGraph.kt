25aa321f4d30e187bd0c82f8a182118f97973463
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-04-06 22:48:24 +0530

    added navbar screens items in AuthNavGraph.kt

--- app/src/main/java/com/vayo/fitcheq/AuthNavGraph.kt
+++ app/src/main/java/com/vayo/fitcheq/AuthNavGraph.kt
@@ -5,21 +5,24 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.vayo.fitcheq.screens.LoginScreen
-import com.vayo.fitcheq.screens.SignUpScreen
-import com.vayo.fitcheq.AuthViewModel
+import com.vayo.fitcheq.screens.auth.LoginScreen
+import com.vayo.fitcheq.screens.auth.SignUpScreen
 import com.vayo.fitcheq.screens.Home.FemaleHomeScreen
 import com.vayo.fitcheq.screens.Home.MaleHomeScreen
-import com.vayo.fitcheq.screens.HomeScreen
-import com.vayo.fitcheq.screens.ProfileScreen
+import com.vayo.fitcheq.screens.auth.ProfileScreen
+import com.vayo.fitcheq.screens.Home.CommunityScreen
+import com.vayo.fitcheq.screens.Home.MyProfileScreen
+import com.vayo.fitcheq.screens.Home.SavedOutfitScreen
 
 sealed class AuthScreen(val route: String) {
     object Login : AuthScreen("login")
     object SignUp : AuthScreen("signup")
-    object Home : AuthScreen("home")
     object UserProfile : AuthScreen("user_profile")
     object MaleHome : AuthScreen("male_home")
     object FemaleHome : AuthScreen("female_home")
+    object Community : AuthScreen("community")
+    object SavedOutfit : AuthScreen("saved_outfit")
+    object MyProfile : AuthScreen("my_profile")
 }
 
 @Composable
@@ -27,9 +30,11 @@ fun AuthNavGraph(navController: NavHostController, authViewModel: AuthViewModel)
     NavHost(navController = navController, startDestination = AuthScreen.Login.route) {
         composable(AuthScreen.Login.route) { LoginScreen(navController, authViewModel) }
         composable(AuthScreen.SignUp.route) { SignUpScreen(navController, authViewModel) }
-        composable(AuthScreen.Home.route) { HomeScreen(navController, authViewModel) }
         composable(AuthScreen.UserProfile.route) { ProfileScreen(navController, authViewModel) }
         composable(AuthScreen.MaleHome.route) { MaleHomeScreen(navController, authViewModel) }
         composable(AuthScreen.FemaleHome.route) { FemaleHomeScreen(navController, authViewModel) }
+        composable(AuthScreen.Community.route) { CommunityScreen(navController, authViewModel) }
+        composable(AuthScreen.SavedOutfit.route) { SavedOutfitScreen(navController, authViewModel) }
+        composable(AuthScreen.MyProfile.route) { MyProfileScreen(navController, authViewModel) }
     }
 }
