2ec1ce76fe6d7084f8150a8b81ca474f88f5cc92
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-04-10 23:11:03 +0530

    Changed name for Occasion data class to FitsCategory.kt + added malefashionList

---
+++ app/src/main/java/com/vayo/fitcheq/data/model/FitsCategory.kt
@@ -0,0 +1,29 @@
+package com.vayo.fitcheq.data.model
+
+data class FitsCategory(val emoji: String, val title: String)
+
+val maleoccasionList = listOf(
+    FitsCategory("📚", "College"),
+    FitsCategory("❤️", "Date"),
+    FitsCategory("🌃", "Nightout"),
+    FitsCategory("💼", "Office"),
+    FitsCategory("🏋️", "Gym")
+)
+
+val femaleoccasionList = listOf(
+    FitsCategory("📚", "FemaleCollege"),
+    FitsCategory("❤️", "Date"),
+    FitsCategory("🌃", "Nightout"),
+    FitsCategory("💼", "Office"),
+    FitsCategory("🏋️", "Gym")
+)
+
+val malefashionList = listOf(
+    FitsCategory("🕊️", "Minimalist"),
+    FitsCategory("💸", "Old Money"),
+    FitsCategory("🌟", "Starboy"),
+    FitsCategory("🧢", "Streetwear"),
+    FitsCategory("🧛", "Dark Academia"),
+    FitsCategory("💿", "Y2K"),
+    FitsCategory("🌻", "Soft Boy"),
+)
