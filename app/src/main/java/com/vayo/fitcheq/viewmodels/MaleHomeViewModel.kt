bce6cc44e12f5a8e71d1952d1216512778f18398
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-04-14 23:29:40 +0530

    added logic to fetch starboy outfits in male home

--- app/src/main/java/com/vayo/fitcheq/viewmodels/MaleHomeViewModel.kt
+++ app/src/main/java/com/vayo/fitcheq/viewmodels/MaleHomeViewModel.kt
@@ -1,4 +1,14 @@
 package com.vayo.fitcheq.viewmodels
 
-class MaleHomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MaleHomeViewModel: ViewModel() {
+    private val firestore = FirebaseFirestore.getInstance()
+
+    //fetch star boy outfits
+//    Firebase.firestore.collection("outfits")
+//    .whereEqualTo("gender", "male")
+//    .whereArrayContains("tags", "starboy")
+//    .get()
 }
