6941a785d898b6180f0f98e6f05c16d252fc7bb0
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-04-23 23:16:08 +0530

    added maleViewModel param and OutfitDetailScreen.kt route in AuthNavGraph.kt

--- app/src/main/java/com/vayo/fitcheq/AuthNavGraph.kt
+++ app/src/main/java/com/vayo/fitcheq/AuthNavGraph.kt
@@ -3,8 +3,10 @@ package com.vayo.fitcheq
 //This will define the navigation routes
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.vayo.fitcheq.screens.auth.LoginScreen
 import com.vayo.fitcheq.screens.auth.SignUpScreen
 import com.vayo.fitcheq.screens.Home.FemaleHomeScreen
@@ -13,6 +15,9 @@ import com.vayo.fitcheq.screens.auth.ProfileScreen
 import com.vayo.fitcheq.screens.Home.CommunityScreen
 import com.vayo.fitcheq.screens.Home.MyProfileScreen
 import com.vayo.fitcheq.screens.Home.SavedOutfitScreen
+import com.vayo.fitcheq.screens.Home.OutfitDetailsScreen
+import com.vayo.fitcheq.viewmodels.AuthViewModel
+import com.vayo.fitcheq.viewmodels.MaleHomeViewModel
 
 sealed class AuthScreen(val route: String) {
     object Login : AuthScreen("login")
@@ -23,10 +28,17 @@ sealed class AuthScreen(val route: String) {
     object Community : AuthScreen("community")
     object SavedOutfit : AuthScreen("saved_outfit")
     object MyProfile : AuthScreen("my_profile")
+    object OutfitDetails : AuthScreen("outfit_details/{gender}/{tag}") {
+        fun createRoute(gender: String, tag: String): String = "outfit_details/$gender/$tag"
+    }
 }
 
 @Composable
-fun AuthNavGraph(navController: NavHostController, authViewModel: AuthViewModel) {
+fun AuthNavGraph(
+    navController: NavHostController, 
+    authViewModel: AuthViewModel,
+    maleViewModel: MaleHomeViewModel
+) {
     NavHost(navController = navController, startDestination = AuthScreen.Login.route) {
         composable(AuthScreen.Login.route) { LoginScreen(navController, authViewModel) }
         composable(AuthScreen.SignUp.route) { SignUpScreen(navController, authViewModel) }
@@ -36,5 +48,21 @@ fun AuthNavGraph(navController: NavHostController, authViewModel: AuthViewModel)
         composable(AuthScreen.Community.route) { CommunityScreen(navController, authViewModel) }
         composable(AuthScreen.SavedOutfit.route) { SavedOutfitScreen(navController, authViewModel) }
         composable(AuthScreen.MyProfile.route) { MyProfileScreen(navController, authViewModel) }
+        composable(
+            route = AuthScreen.OutfitDetails.route,
+            arguments = listOf(
+                navArgument("gender") { type = NavType.StringType },
+                navArgument("tag") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val gender = backStackEntry.arguments?.getString("gender") ?: "Male"
+            val tag = backStackEntry.arguments?.getString("tag") ?: "default"
+
+            OutfitDetailsScreen(
+                gender = gender,
+                tag = tag,
+                viewModel = maleViewModel
+            )
+        }
     }
 }
