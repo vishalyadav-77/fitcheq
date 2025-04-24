155be28797820279ebeebcc8ecc54458895363aa
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-04-24 23:53:23 +0530

    Added link and website field

--- app/src/main/java/com/vayo/fitcheq/data/model/OutfitData.kt
+++ app/src/main/java/com/vayo/fitcheq/data/model/OutfitData.kt
@@ -2,9 +2,11 @@ package com.vayo.fitcheq.data.model
 
 data class OutfitData(
     val id: String = "",
+    val link: String ="",
     val imageUrl: String = "",
     val title: String = "",
     val price: String = "",
+    val website: String = "",
     val gender: String = "",
     val tags: List<String> = emptyList()
 )
