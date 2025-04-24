035f3eb511dc19dfedd4d53606a6c5bb50ab7036
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-04-24 23:52:59 +0530

    Added navigation to external site + marquee effect in title + statusBar padding

--- app/src/main/java/com/vayo/fitcheq/screens/Home/OutfitDetailScreen.kt
+++ app/src/main/java/com/vayo/fitcheq/screens/Home/OutfitDetailScreen.kt
@@ -1,15 +1,23 @@
 package com.vayo.fitcheq.screens.Home
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -31,7 +39,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,10 +56,12 @@ import com.vayo.fitcheq.viewmodels.MaleHomeViewModel
 
 //@Preview
 @Composable
-fun OutfitDetailsScreen(navController: NavController, viewModel: MaleHomeViewModel){
+fun OutfitDetailsScreen(gender: String,tag: String,viewModel: MaleHomeViewModel){
+
+    val context = LocalContext.current
     // Fetch only once when screen loads
-    LaunchedEffect(Unit) {
-        viewModel.fetchOutfitsByTagAndGender("starboy", "male")
+    LaunchedEffect(gender, tag) {
+        viewModel.fetchOutfitsByTagAndGender(tag, gender)
     }
     val outfits = viewModel.outfits.collectAsState().value
     val isLoading = viewModel.isLoading.collectAsState().value
@@ -57,17 +70,20 @@ fun OutfitDetailsScreen(navController: NavController, viewModel: MaleHomeViewMod
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(vertical = 16.dp)
+            .padding(
+                top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding() + 16.dp,
+                start = 6.dp,
+                end = 6.dp
+            )
     ) {
         // Title section
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
+                .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Starboy Collection",
+                text = "${tag.replaceFirstChar { it.uppercaseChar() }} Collection",
                 fontSize = 22.sp,
                 fontWeight = FontWeight.SemiBold
             )
@@ -119,7 +135,7 @@ fun OutfitDetailsScreen(navController: NavController, viewModel: MaleHomeViewMod
                     columns = GridCells.Fixed(2),
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(horizontal = 4.dp),
+                        .padding(horizontal = 0.dp),
                     horizontalArrangement = Arrangement.spacedBy(4.dp),
                     verticalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
@@ -128,11 +144,15 @@ fun OutfitDetailsScreen(navController: NavController, viewModel: MaleHomeViewMod
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .height(350.dp)
-                                .padding(0.dp),
+                                .padding(2.dp)
+                                .clickable {
+                                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(outfit.link))
+                                    context.startActivity(intent)
+                                },
                             elevation = CardDefaults.cardElevation(4.dp),
                             shape = RoundedCornerShape(12.dp)
                         ) {
-                            Column(
+                            Column(                                      //two card in a same row
                                 modifier = Modifier.padding(8.dp),
                                 horizontalAlignment = Alignment.CenterHorizontally
                             ) {
@@ -146,16 +166,42 @@ fun OutfitDetailsScreen(navController: NavController, viewModel: MaleHomeViewMod
                                         .clip(RoundedCornerShape(8.dp))
                                 )
                                 Spacer(modifier = Modifier.height(8.dp))
-                                Text(
-                                    text = outfit.title,
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 14.sp
-                                )
-                                Text(
-                                    text = "₹${outfit.price}",
-                                    color = Color.Cyan,
-                                    fontSize = 14.sp
-                                )
+                                Column(
+                                    modifier = Modifier.fillMaxWidth()
+                                        .padding(horizontal = 4.dp),
+                                    verticalArrangement = Arrangement.spacedBy(2.dp)
+                                ) {
+                                    Text(
+                                        text = outfit.title,
+                                        fontWeight = FontWeight.SemiBold,
+                                        fontSize = 14.sp,
+                                        maxLines = 1,
+                                        overflow = TextOverflow.Ellipsis, // fallback
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .basicMarquee()
+                                            .focusable() // required for marquee to work
+                                    )
+
+                                    Spacer(modifier = Modifier.width(4.dp))
+
+                                    Text(
+                                        text = "₹${outfit.price}",
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 14.sp,
+                                    )
+                                    Row(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        horizontalArrangement = Arrangement.End
+                                    ) {
+                                        Text(
+                                            text = outfit.website,
+                                            fontSize = 12.sp,
+                                            fontStyle = FontStyle.Italic,
+                                            color = Color.DarkGray
+                                        )
+                                    }
+                                }
                             }
                         }
                     }
