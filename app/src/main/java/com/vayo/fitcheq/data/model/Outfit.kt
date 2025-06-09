fb1321c08c6119f595a9ed75da3d56acbd140f75
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-06-09 14:21:59 +0530

    Outfit.kt(debug purpose) updated(not fixed)

---
+++ app/src/main/java/com/vayo/fitcheq/data/model/Outfit.kt
@@ -0,0 +1,11 @@
+package com.vayo.fitcheq.data.model
+
+data class Outfit(
+    val id: String,
+    val title: String,
+    val imageUrl: String,
+    val price: String,
+    val website: String,
+    val link: String
+)
+
