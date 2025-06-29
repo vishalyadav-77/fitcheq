b41ff5df270d349b99ce1d0804d59b579d98be9b
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-06-30 02:23:06 +0530

    updates assets for occasion section

--- app/src/main/java/com/vayo/fitcheq/data/model/FitsCategory.kt
+++ app/src/main/java/com/vayo/fitcheq/data/model/FitsCategory.kt
@@ -3,12 +3,12 @@ package com.vayo.fitcheq.data.model
 data class FitsCategory( val emoji: String? = null, val imageUrl: String? = null, val title: String)
 
 val maleoccasionList = listOf(
-    FitsCategory(title = "College", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/office_male.webp"),
-    FitsCategory(title = "Date", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/date_male.webp"),
-    FitsCategory(title = "Beach", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/beach_male.webp"),
-    FitsCategory(title = "Gym", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/gym_male.webp"),
-    FitsCategory(title = "Wedding", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/date_male.webp"),
-    FitsCategory(title = "Office", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/office_male.webp"),
+    FitsCategory(title = "College", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/college_c.webp"),
+    FitsCategory(title = "Date", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/date_c.webp"),
+    FitsCategory(title = "Beach", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/beach_c.webp"),
+    FitsCategory(title = "Gym", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/gym_c.webp"),
+    FitsCategory(title = "Wedding", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/party_c.webp"),
+    FitsCategory(title = "Office", imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/occasion-images/office_c.webp"),
 )
 val malecategoryList = listOf(
     FitsCategory(imageUrl = "https://cdn.jsdelivr.net/gh/vishalyadav-77/fitcheq-assests/categories-assets/tshirt_man.webp", title =  "Tshirt"),
