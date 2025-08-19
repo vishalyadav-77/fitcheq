7741dad8f6bab3b52d48a570d3dd380a47e84b9b
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-08-20 01:43:36 +0530

    created FitGuideCategory.kt data class

---
+++ app/src/main/java/com/vayo/fitcheq/data/model/FitGuideCategory.kt
@@ -0,0 +1,27 @@
+package com.vayo.fitcheq.data.model
+
+import kotlinx.serialization.Serializable
+
+// Wrapper for root
+@Serializable
+data class FitGuideWrapper(
+    val guides: List<FitGuide>
+)
+
+@Serializable
+data class FitGuideCategory(
+    val id: String,
+    val title: String,
+    val summary: String,
+    val image: String,
+    val details: List<String>,
+    val proTip: String
+)
+
+@Serializable
+data class FitGuide(
+    val gender: String,
+    val height: HeightGroup,
+    val bodyType: BodyType,
+    val categories: List<FitGuideCategory>
+)
