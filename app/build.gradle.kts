c3139709b7ff2339d95a683dd7eddef01309e76a
vishalyadav-77 <vishalyadav15044@gmail.com>
2025-10-29 13:25:14 +0530

    added app update + material dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,10 @@ dependencies {
     implementation ("com.google.code.gson:gson:2.11.0")
     implementation ("androidx.core:core-splashscreen:1.0.1")
 
+    implementation("com.google.android.play:app-update:2.1.0")
+    implementation("com.google.android.play:app-update-ktx:2.1.0")
+    implementation("com.google.android.material:material:1.12.0")
+
 
 
 
